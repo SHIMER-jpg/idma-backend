949b4317191e7e732769c1dabb7880d63fe4da59
Santiago Diaz <santiago.diaz@venturatravel.org>
2023-10-22 12:30:28 -0300

    pasted from nestjs starter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot() ,OrdersModule, ProductsModule],
+  imports: [ConfigModule.forRoot(), OrdersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
