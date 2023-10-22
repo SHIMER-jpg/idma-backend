20c28f3c6bb9ffea5db5786c3bcca08df422428c
Santiago Diaz <santiago.diaz@venturatravel.org>
2023-10-22 13:09:09 -0300

    fixed dto

--- src/infra/tiendanube/EventDto.ts
+++ src/infra/tiendanube/EventDto.ts
@@ -1,29 +1,31 @@
-import { IsInt, IsNotEmpty, IsIn } from "class-validator"
+import { IsInt, IsNotEmpty, IsIn, IsString } from 'class-validator';
 
 export class TiendaNubeEventDto {
-    @IsInt()
-    @IsNotEmpty()
-    id: number;
+  @IsInt()
+  @IsNotEmpty()
+  id: number;
 
-    @IsNotEmpty()
-    @IsIn([ 'app/uninstalled'
-    ,'cart/created'
-    ,'cart/updated'
-    ,'category/created'
-    ,'category/updated'
-    ,'category/deleted'
-    ,'order/created'
-    ,'order/updated'
-    ,'order/packed'
-    ,'order/paid'
-    ,'order/fulfilled'
-    ,'order/cancelled'
-    ,'product/created'
-    ,'product/updated'
-    ,'product/deleted'])
-    event: string;
+  @IsNotEmpty()
+  @IsIn([
+    'app/uninstalled',
+    'cart/created',
+    'cart/updated',
+    'category/created',
+    'category/updated',
+    'category/deleted',
+    'order/created',
+    'order/updated',
+    'order/packed',
+    'order/paid',
+    'order/fulfilled',
+    'order/cancelled',
+    'product/created',
+    'product/updated',
+    'product/deleted',
+  ])
+  event: string;
 
-    @IsInt()
-    @IsNotEmpty()
-    store_id: number;
+  @IsString()
+  @IsNotEmpty()
+  store_id: number;
 }
